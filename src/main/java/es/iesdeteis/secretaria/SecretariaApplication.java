39c6e2db250ecdbcb4aa9cd7cda0c9f37e533001
Francisco Javier Rodríguez Suarez <a25franciscojrs@teis.intranet>
2026-04-13 19:30:13 +0200

    Base inicial del proyecto secretaria

---
+++ src/main/java/es/iesdeteis/secretaria/SecretariaApplication.java
@@ -0,0 +1,13 @@
+package es.iesdeteis.secretaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretariaApplication.class, args);
+	}
+
+}
